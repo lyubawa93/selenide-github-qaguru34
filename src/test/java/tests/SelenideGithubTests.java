ea36879cb559a8a525ec1dac8ae633720deba28e
lybov <lyuba.n3@gmail.com>
2025-04-10 22:38:37 +0300

    fixed the mistake

--- src/test/java/tests/SelenideGithubTests.java
+++ src/test/java/tests/SelenideGithubTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -26,7 +24,17 @@ public class SelenideGithubTests {
         $("#wiki-pages-box").$(byText("Show 3 more pages…")).click();
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        $(byTagAndText("h4","3. Using JUnit5 extend test class:")).shouldBe(visible);
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 }
