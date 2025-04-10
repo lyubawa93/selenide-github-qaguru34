c042f4e64774332bd531c25f1410ab6fadef3353
lybov <lyuba.n3@gmail.com>
2025-04-10 15:20:48 +0300

    added SelenideGithubTests

---
+++ src/test/java/tests/SelenideGithubTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGithubTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000; // default 4000
+    }
+    @Test
+    void checkSoftAssertionsPage() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(byText("Show 3 more pages…")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(byTagAndText("h4","3. Using JUnit5 extend test class:")).shouldBe(visible);
+
+    }
+}
