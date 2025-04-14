3063fa59bb684d0ba83ef85844a2f6dd4a8aa3db
lybov <lyuba.n3@gmail.com>
2025-04-14 14:52:44 +0300

    Added SelenideGithubTest

---
+++ src/test/java/tests/SelenideGithubTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGithubTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000; // default 4000
+}
+    @Test
+    void solutionsEnterprizeTest() {
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+
+    }
+}
